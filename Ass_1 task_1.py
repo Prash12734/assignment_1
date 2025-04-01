1aeffac3a452c21021e4f1dbffc46e6e5bd113ce
Prash12734 <prashantwmr92@gmail.com>
2025-04-01 21:05:40 +0530

    Add files via upload

---
+++ Ass_1 task_1.py
@@ -0,0 +1,13 @@
+# Basic Mathematical Operations
+a=input('Enter the first number :')
+b=input('Enter the second number: ')
+a=int(a)
+b=int(b)
+c=int(a+b)
+d=int(a-b)
+e=int(a*b)
+f=float(a/b)
+print('Addition: ' , c)
+print('Subtraction: ', d)
+print('Multiplication: ' , e)
+print('Division: ' , f)
---
+++ Ass_2 task_2.py
@@ -0,0 +1,6 @@
+#  Personalized Greetings
+
+a=input('Enter your first name: ')
+b=input('Enter your last name: ')
+
+print("Hello " + a  +  b +   "! Welcome to the Python program.")
